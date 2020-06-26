379fb7651efb890b0ba73a7c73a9f42e62fcc23f
Tikha <64693040+Sub-Olahoop@users.noreply.github.com>
2020-06-26 22:24:19 +0800

    Create Server_Pixel.js

---
+++ Server_Pixel.js
@@ -0,0 +1,3 @@
+{ "data": [ { "event_name": "ViewContent", "event_time": 1593162366, "event_id": "event.id.123", "event_source_url": "http:\/\/jaspers-market.com", "user_data": { "client_ip_address": "1.2.3.4", "client_user_agent": "test user agent" } } ], "test_event_code": "113984427014056" }
+
+
